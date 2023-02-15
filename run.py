54477e44579a688fa9fd4ee3fc5848c1b561ecc4
Sergii Kostanets <sergiikostanets@gmail.com>
2023-02-15 12:24:18 +0000

    getting electricity data from the user

--- run.py
+++ run.py
@@ -13,8 +13,45 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('codeinstitute-utilities')
 
 
-electricity = SHEET.worksheet('electricity')
+# electricity = SHEET.worksheet('electricity')
 
-data = electricity.get_all_values()
+# data = electricity.get_all_values()
 
-print(data)
+# print(data)
+
+def get_electricity_data():
+    """
+    """
+    # while True:
+    print("\nPlease enter electricity data.")
+    print("Data should be meter reading, data and price per kWh, separated by commas.")
+    print("Example: 30120.5,15.02.2023,0.42\n")
+
+    data_str = input("Enter your data here:\n")
+
+    electricity_data = data_str.split(",")
+
+        # if validate_data(electricity_data):
+        #     print("Data is valid!")
+        #     break
+
+    return electricity_data
+
+
+def update_worksheet_electricity(data):
+    """
+    """
+    print(f"Updating electricity worksheet...\n")
+    worksheet_to_update = SHEET.worksheet('electricity')
+    worksheet_to_update.append_row(data)
+    print(f"Electricity worksheet updated successfully.\n")
+
+
+def main():
+    """
+    """
+    data = get_electricity_data()
+    update_worksheet_electricity(data)
+
+
+main()
